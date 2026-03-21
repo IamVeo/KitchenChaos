e9aa65bca64dffd4d8b39f8762afbd2c439f321a
Đinh Gia Bảo <bao03112005@gmail.com>
2026-03-21 17:58:45 +0700

    add attack delay

--- Assets/Scripts/ScriptableObjects/PlayerDataSO.cs
+++ Assets/Scripts/ScriptableObjects/PlayerDataSO.cs
@@ -3,6 +3,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PlayerDataSO", menuName = "ScriptableObjects/PlayerDataSO")]
 public class PlayerDataSO : ScriptableObject
 {
-    public float moveSpeed = 7f;
-    public float attackDelay = 0.3f;
+    public float moveSpeed;
+    public float attackDelay;
 }
